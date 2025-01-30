139e9eb5be4a7217dec01d4874bb31de49abc5e2
Reda Ennakouri <reda.ennakouri@gmail.com>
2025-01-31 00:04:04 +0100

    feat : create File Route for front side

---
+++ resources/js/routes.js
@@ -0,0 +1,17 @@
+const films = {
+    index: 'films.index',
+    show: 'films.show',
+    edit: 'films.edit',
+    update: 'films.update',
+    destroy: 'films.destroy',
+};
+const api = {
+    films: {
+        index: 'api.films.index',
+    },
+};
+
+export const routes = {
+    films,
+    api,
+};
